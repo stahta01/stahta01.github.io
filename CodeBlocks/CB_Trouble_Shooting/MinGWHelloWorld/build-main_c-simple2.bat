7f222c011d86fd7ae810f5420f9d1b55524c9ea3
Tim S <stahta01@users.sourceforge.net>
2015-01-04 13:13:02 -0500

    Added deleted section to C and added simple2 bats.

--- CodeBlocks/CB_Trouble_Shooting/MinGWHelloWorld/build-main_c-simple2.bat
+++ CodeBlocks/CB_Trouble_Shooting/MinGWHelloWorld/build-main_c-simple2.bat
@@ -6,7 +6,7 @@ SET CB_GCC_TOOL_GCC=mingw32-gcc.exe
 SET CB_GCC_TOOL_GXX=mingw32-g++.exe
 SET CB_GCC_TOOL_LD=mingw32-g++.exe
 
-SET PATH=%CB_GCC_TOOLCHAIN%\bin;%PATH%
+SET PATH=%CB_GCC_TOOLCHAIN%\bin
 
 if not exist "bin"            mkdir bin
 if not exist "bin\DebugWin32" mkdir bin\DebugWin32
--- CodeBlocks/CB_Trouble_Shooting/MinGWHelloWorld/build-main_cpp-simple2.bat
+++ CodeBlocks/CB_Trouble_Shooting/MinGWHelloWorld/build-main_cpp-simple2.bat
@@ -6,7 +6,7 @@ SET CB_GCC_TOOL_GCC=mingw32-gcc.exe
 SET CB_GCC_TOOL_GXX=mingw32-g++.exe
 SET CB_GCC_TOOL_LD=mingw32-g++.exe
 
-SET PATH=%CB_GCC_TOOLCHAIN%\bin;%PATH%
+SET PATH=%CB_GCC_TOOLCHAIN%\bin
 
 if not exist "bin"            mkdir bin
 if not exist "bin\DebugWin32" mkdir bin\DebugWin32
@@ -16,8 +16,8 @@ if not exist "obj\DebugWin32" mkdir obj\DebugWin32
 if exist "obj\DebugWin32\main.o" del obj\DebugWin32\main.o
 if exist "bin\DebugWin32\HelloWorld.exe" del bin\DebugWin32\HelloWorld.exe
 
-%CB_GCC_TOOL_GCC% -Wall -fexceptions -g -c main.c -o obj\DebugWin32\main.o
-%CB_GCC_TOOL_GXX%  -o bin\DebugWin32\HelloWorld.exe obj\DebugWin32\main.o
+%CB_GCC_TOOL_GXX% -Wall -fexceptions -g -c main.cpp -o obj\DebugWin32\main.o
+%CB_GCC_TOOL_LD%  -o bin\DebugWin32\HelloWorld.exe obj\DebugWin32\main.o
 
 bin\DebugWin32\HelloWorld.exe
 
